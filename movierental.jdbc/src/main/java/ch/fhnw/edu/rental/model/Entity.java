85f27c1f785821194311e1bfdd38f273cc8277b2
Lüscher Pascal <luescherpascal@gmail.com>
2017-10-09 09:12:09 +0200

    minor bugfixes

--- movierental.jdbc/src/main/java/ch/fhnw/edu/rental/model/Entity.java
+++ movierental.jdbc/src/main/java/ch/fhnw/edu/rental/model/Entity.java
@@ -1,5 +1,7 @@
 package ch.fhnw.edu.rental.model;
 
 public interface Entity {
-	public Long getId();
+    public Long getId();
+
+    public void setId(Long id);
 }
--- movierental.jdbc/src/main/java/ch/fhnw/edu/rental/persistence/impl/JDBCBaseClass.java
+++ movierental.jdbc/src/main/java/ch/fhnw/edu/rental/persistence/impl/JDBCBaseClass.java
@@ -10,52 +10,54 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import ch.fhnw.edu.rental.model.Entity;
 
 public abstract class JDBCBaseClass<T extends Entity> {
-	protected String tableName;
-	protected String identtyFieldname;
+    protected String tableName;
+    protected String identtyFieldname;
 
-	@Autowired
-	protected JdbcTemplate template;
+    @Autowired
+    protected JdbcTemplate template;
 
-	public T findOne(Long id) {
-		if (id == null)
-			throw new IllegalArgumentException();
-		return template.query("select * from " + tableName + " where " + identtyFieldname + " = ?",
-				(rs, row) -> createEntity(rs), id).get(0);
-	}
+    public T findOne(Long id) {
+        if (id == null)
+            throw new IllegalArgumentException();
+        return template.query("select * from " + tableName + " where " + identtyFieldname + " = ?",
+                (rs, row) -> createEntity(rs), id).get(0);
+    }
 
-	protected abstract T createEntity(ResultSet rs) throws SQLException;
+    protected abstract T createEntity(ResultSet rs) throws SQLException;
 
-	public List<T> findAll() {
-		return template.query("select * from " + tableName, (rs, row) -> createEntity(rs));
-	}
+    public List<T> findAll() {
+        return template.query("select * from " + tableName, (rs, row) -> createEntity(rs));
+    }
 
-	public void delete(T t) {
-		if (t == null)
-			throw new IllegalArgumentException();
-		delete(t.getId());
-	}
+    public void delete(T t) {
+        if (t == null)
+            throw new IllegalArgumentException();
+        delete(t.getId());
+        t.setId(null);
+    }
 
-	public void delete(Long id) {
-		if (id == null)
-			throw new IllegalArgumentException();
+    public void delete(Long id) {
+        if (id == null)
+            throw new IllegalArgumentException();
 
-		template.update("DELETE FROM " + tableName + " WHERE " + identtyFieldname + " = ?", id);
-	}
+        template.update("DELETE FROM " + tableName + " WHERE " + identtyFieldname + " = ?", id);
+        
+    }
 
-	public boolean exists(Long id) {
-		if (id == null) {
-			return false;
-		}
-		return !template.query("SELECT TOP 1 * FROM " + tableName + " WHERE " + identtyFieldname + " = ?",
-				(rs, row) -> createEntity(rs), id).isEmpty();
-	}
+    public boolean exists(Long id) {
+        if (id == null) {
+            return false;
+        }
+        return !template.query("SELECT TOP 1 * FROM " + tableName + " WHERE " + identtyFieldname + " = ?",
+                (rs, row) -> createEntity(rs), id).isEmpty();
+    }
 
-	public long count() {
-		return template.query("SELECT COUNT(*) as Cnt FROM " + tableName, (rs, row) -> rs.getLong("Cnt")).get(0);
-	}
+    public long count() {
+        return template.query("SELECT COUNT(*) as Cnt FROM " + tableName, (rs, row) -> rs.getLong("Cnt")).get(0);
+    }
 
-	protected long getLastUsedId() {
-		return template.query("SELECT MAX(" + identtyFieldname + ") as maxId FROM " + tableName,
-				(rs, row) -> rs.getLong("maxId")).get(0);
-	}
+    protected long getLastUsedId() {
+        return template.query("SELECT MAX(" + identtyFieldname + ") as maxId FROM " + tableName,
+                (rs, row) -> rs.getLong("maxId")).get(0);
+    }
 }
