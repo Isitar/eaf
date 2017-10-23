00473afc2fd561ab1abb3c17de1cf6b9c383b81f
Lüscher Pascal <luescherpascal@gmail.com>
2017-10-23 10:00:01 +0200

    Added jsql land inheritance excercise

---
+++ lab-jpa-inheritance/src/main/java/ch/fhnw/jpa/inheritance/Topic.java
@@ -0,0 +1,36 @@
+package ch.fhnw.jpa.inheritance;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Topic {
+	@Id @GeneratedValue
+	private Long id;
+	private String title;
+	private String owner;
+	
+	protected Topic() { }
+	
+	public Topic(String title, String owner) {
+		this.title = title;
+		this.owner = owner;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+	
+}
