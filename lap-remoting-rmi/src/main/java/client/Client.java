109b1f0033f1dd6fdc4b51f4a0951bee893e6dcc
pascal lüscher <pascal@debian-palu>
2017-12-11 09:19:27 +0100

    use remoteinvocation directly

--- lap-remoting-rmi/src/main/java/client/Client.java
+++ lap-remoting-rmi/src/main/java/client/Client.java
@@ -1,12 +1,18 @@
 package client;
 
+import java.rmi.Naming;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.remoting.rmi.RmiInvocationHandler;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
+import org.springframework.remoting.support.RemoteInvocation;
 
+import bank.Account;
 import bank.AccountService;
 
 @SpringBootApplication
@@ -27,10 +33,29 @@ public class Client {
 
 	@Bean
 	CommandLineRunner run(AccountService service) {
+		service.insertAccount(new Account("Pascal"));
+		service.insertAccount(new Account("Pascal"));
+		service.insertAccount(new Account("Pascal"));
+		service.insertAccount(new Account("Pascal"));
+		System.out.println("Pascal Accounts (should be n*4): " + service.getAccounts("Pascal").size());
+
 		return args -> {
 			System.out.println("Client started");
-			
-			Object proxy = service;
+			Object proxy = Naming.lookup("rmi://localhost:1099/AccountService");
+
+			org.springframework.remoting.rmi.RmiInvocationHandler rmiHandler = (org.springframework.remoting.rmi.RmiInvocationHandler) proxy;
+
+			RemoteInvocation inv = new RemoteInvocation();
+			inv.setMethodName("getAccounts");
+			inv.setParameterTypes(new Class[] { String.class });
+			inv.setArguments(new Object[] { "Pascal" });
+			proxy = rmiHandler.invoke(inv);
+			java.util.List<Account> accs = (List<Account>) proxy;
+			for (Account account : accs) {
+				System.out.println(account.getName());
+			}
+
+			// Object proxy = service;
 			System.out.println("proxy.getClass() = " + proxy.getClass().getCanonicalName());
 			System.out.println("implemented interfaces:");
 			for (Class<?> c : proxy.getClass().getInterfaces()) {
@@ -46,7 +71,7 @@ public class Client {
 		proxy.setServiceUrl(url);
 		proxy.setServiceInterface(AccountService.class);
 		proxy.afterPropertiesSet();
-		return (AccountService)proxy.getObject();
+		return (AccountService) proxy.getObject();
 	}
 
 }
